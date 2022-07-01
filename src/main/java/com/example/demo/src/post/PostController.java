a06fb094b027ff4f5fcffe429f081121535d4045
tanggu01 <tanggu1221@gmail.com>
2022-07-01 14:59:13 +0900

    add 1.3 createPosts api

--- src/main/java/com/example/demo/src/post/PostController.java
+++ src/main/java/com/example/demo/src/post/PostController.java
@@ -4,6 +4,8 @@ import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.config.BaseResponseStatus;
 //import com.example.demo.src.post.model.*;
+import com.example.demo.src.post.model.PostPostsReq;
+import com.example.demo.src.post.model.PostPostsRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,7 +34,24 @@ public class PostController {
         this.jwtService = jwtService;
     }
 
-
+    /**
+     * 1.3 게시물 생성 api : /post
+     * @param postPostsReq
+     * @author taehyun
+     */
+    @ResponseBody
+    @PostMapping("")
+    public BaseResponse<PostPostsRes> createPosts(@RequestBody PostPostsReq postPostsReq) {
+        try { //형식적 validation
+            if (postPostsReq.getContent().length() > 500) {
+                return new BaseResponse<>(BaseResponseStatus.POST_POSTS_INVALID_CONTENTS);
+            }
+            PostPostsRes postPostsRes = postService.createPosts(postPostsReq);
+            return new BaseResponse<>(postPostsRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
 
 }
 
